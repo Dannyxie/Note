eaa091275391f5132c9f8ca3469fd8ad1b909467
Deryck <285205451@qq.com>
2015-05-14 00:49:18 +0800

    add Note of Angular.js

---
+++ Note of Angular.js
@@ -0,0 +1,19 @@
+#Angular.js
+
+##Chapter 2
+- The `ng-app` derective lets you tell Angular whick part of your page it should expect to manage( we can manage only a part of the page)
+
+###Model View Controller
+1. A model containing data that represents current state of your application.
+2. Views that display this data.
+3. Controllers manage the relationship between the model and the views.
+
+
+###Diplay Text
+- `ng-bind`
+
+###Form Input
+- `ng-model`
+- `ng-change`
+
+
